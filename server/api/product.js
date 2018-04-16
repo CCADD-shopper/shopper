70c3ad9bba42feb69f2230d4faf962e2dd792401
Dan Feeney <dfeeney2456@gmail.com>
2018-04-16 17:17:45 -0500

    try 2

--- server/api/product.js
+++ server/api/product.js
@@ -2,3 +2,32 @@ const router = require('express').Router()
 const {Product} = require('../db/product')
 module.exports = router
 
+router.param('productId', (req, res, next, productId) => {
+    Product.findById(productId)
+    .then(product => {
+        if (!product) res.sendStatus(404);
+        req.product = product;
+        next();
+    })
+    .catch(next);
+})
+
+router.get('/', (req, res, next) => {
+    Product.findAll({})
+      .then(users => res.json(users))
+      .catch(next)
+  })
+
+  router.post('/', (req, res, next) => {
+    Product.findAll({})
+      .then(users => res.json(users))
+      .catch(next)
+  })
+
+  router.put('/', (req, res, next) => {
+    req.product.update(req.body)
+    .then(product => product.reload(Product))
+    .then(updatedProduct => res.json(updatedProduct))
+    .catch(next);
+  })
+
