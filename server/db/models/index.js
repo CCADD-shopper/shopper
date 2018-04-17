3db2dee3a10af73b311ed2965b2ca045d4fd82d1
Cody Fayolle <cfayolle@umich.edu>
2018-04-17 12:03:08 -0500

    closes #2

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,7 @@ const User = require('./user')
 const Category = require('./category')
 const Product = require('./product')
 const Review = require('./review')
+const Order = require('./order')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -21,4 +22,5 @@ module.exports = {
   Category,
   Product,
   Review,
+  Order,
 }
