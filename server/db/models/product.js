8eb8464b384566b633a452172961076552de087c
David Fudacz <davidfudacz@gmail.com>
2018-04-18 15:10:41 -0500

    pulling in new master

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,6 +1,7 @@
 
 const Sequelize = require('sequelize')
 const db = require('../db')
+const Review = require('./review');
 
 const Product = db.define('product', {
   name: {
@@ -33,6 +34,33 @@ const Product = db.define('product', {
     },
     defaultValue: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Tags_font_awesome.svg',
   },
+  reviewRating: {
+    type: Sequelize.INTEGER,
+    get () {
+      function round (number, precision) {
+        var shift = function (number, precision, reverseShift) {
+          if (reverseShift) {
+            precision = -precision;
+          }  
+          var numArray = ("" + number).split("e");
+          return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+        };
+        return shift(Math.round(shift(number, precision, false)), precision, true);
+      }
+
+      const reviews = Review.findAll({
+        where: {
+          productId: this.getDataValue('id'),
+        }
+      })
+      const rating = reviews.reduce((acc, curVal) => {
+        return acc + curVal
+      }, 0) / reviews.length;
+
+      return round(rating, 1)
+
+    }
+  }
 })
 
 module.exports = Product;
