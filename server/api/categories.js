602aa159bd265ded0dd44fc19fb0b5da42551990
Cody Fayolle <cfayolle@umich.edu>
2018-04-18 15:57:12 -0500

    closes #75

--- server/api/categories.js
+++ server/api/categories.js
@@ -9,16 +9,32 @@ router.get('/', (req, res, next) => {
 
 router.post('/new-category', (req, res, next) => {
     Category.create(req.body)
-    .then(newCat => res.send(200).json(newCat))
+    .then(newCat => res.json(newCat))
     .catch(next);
 })
 
-router.delete('/:id', async (req, res, next) => {
+router.put('/:categoryId', async (req, res, next) => {
+  try {
+    const id = req.params.categoryId
+    const updatedCategory = await Category.update(req.body, {where: {id}, returning: true})
+    if (updatedCategory[0] === 0) {
+      const error = new Error('hello')
+      error.status = 404;
+      throw error
+    }
+    res.json(updatedCategory[1][0])
+  }
+  catch (err) {
+    console.log('hi')
+    next(err)
+  }
+})
+
+router.delete('/:categoryId', async (req, res, next) => {
     try {
-      const catId = req.params.id;
-      const category = await Category.findById(catId)
-      category.destroy()
-      res.sendStatus(202)
+      const catId = req.params.categoryId;
+      const deletedCategory = await Category.destroy({where: {id: catId}})
+      res.json(deletedCategory)
     }
     catch (err){
       next(err)
