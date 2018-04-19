5680b1f9f2cf277850a7afdd14df5f9b4613f8fe
Awenman <goullet86@yahoo.com>
2018-04-19 12:18:29 -0500

    looks good fer now

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
+  <div className="navBar">
     <h1>TopShop</h1>
     <nav>
       {isLoggedIn ? (
