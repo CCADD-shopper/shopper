af7f02729bcdf6bec763a894c91ed430ef31eab5
David Fudacz <davidfudacz@gmail.com>
2018-04-19 11:50:19 -0500

    created left sidebar closes #113

---
+++ client/components/category/index.js
@@ -0,0 +1,6 @@
+/**
+ * `components/product/index.js` exists simply as a 'central export' for our product components.
+ * This way, we can import all of our components from the same place, rather than
+ * having to figure out which file they belong to!
+ */
+export {default as CategorySelector} from './category-selector'
