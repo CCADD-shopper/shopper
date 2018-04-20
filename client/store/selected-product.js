bbebc7936da4f8dc95389ae76e654430f9cfc066
dfeeney87 <31283788+dfeeney87@users.noreply.github.com>
2018-04-20 09:26:49 -0500

    Update selected-product.js

--- client/store/selected-product.js
+++ client/store/selected-product.js
@@ -16,7 +16,6 @@ export const getProductFromServerThunkerator = (productId) => {
   return async (dispatch) => {
     try {
       const selectedProduct = await axios.get(`/api/products/${productId}`);
-      console.log(selectedProduct.data);
       dispatch(getProductFromServer(selectedProduct.data));
 
     } catch (err) {
