573d4ea58f1d1411a9e1dc144926b2d27932c280
Dan Feeney <dfeeney2456@gmail.com>
2018-04-23 11:26:25 -0500

    initialize

---
+++ server/db/models/order-details.js
@@ -0,0 +1,70 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const OrderDetails = db.define('order-details', {
+    shipAddress1: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    shipAddress2: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    shipCity: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    shipState: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    shipZip: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+    billAddress1: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    billAddress2: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    billCity: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    billState: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    billZip: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+    payCcType: {
+        type: Sequelize.ENUM,
+        values: ['VISA', 'American Express', 'Mastercard', `Big Joe's Credit Hut`]
+    },
+    payCcNumber: {
+        type: Sequelize.STRING,
+        isCreditCard: true,
+    },
+    payCvcNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    payZip: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    payExpiry: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+})
+
+module.exports = OrderDetails;
