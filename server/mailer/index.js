ec6f004026e7de67a32c45251d98b7f086d96a47
Awenman <goullet86@yahoo.com>
2018-04-24 13:37:09 -0500

    email is working with better structure

--- server/mailer/index.js
+++ server/mailer/index.js
@@ -4,101 +4,58 @@
 
 const nodemailer = require('nodemailer');
 
+let orderedMessage = {
+    // recipients
+    to: 'Andrew <bz61bi+8599paxujhy4w@sharklasers.com>',
+
+    subject: 'Your Order was Recieved!',
+
+    text: 'Thanks for your order with TopShop!',
+
+};
+
+let shippedMessage = {
+    // recipients
+    to: 'Andrew <bz61bi+8599paxujhy4w@sharklasers.com>',
+
+    subject: 'Your Order was shipped!',
+
+    text: 'Thanks for shopping with TopShop!',
+
+}
+
 // Generate SMTP service account from ethereal.email
-nodemailer.createTestAccount((err, account) => {
-    if (err) {
-        console.error('Failed to create a testing account');
-        console.error(err);
+var transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+      user: 'TopShopPlopper@gmail.com',
+      pass: 'TopShopPloppers1'
+  },
+  logger: false,
+  debug: false
+}, {
+    // default message fields
+
+    // sender info
+    from: 'TopShop Ploppers <TopShopPlopper@gmail.com>',
+    headers: {
+        'X-Laziness-level': 1000 // just an example header, no need to use this
+    }
+}
+);
+
+const orderedEmail = transporter.sendMail(orderedMessage, (error, info) => {
+    if (error) {
+        console.log('Error occurred');
+        console.log(error.message);
         return process.exit(1);
     }
 
-    console.log('Credentials obtained, sending message...');
+    console.log('Message sent successfully!')
+    console.log(nodemailer.getTestMessageUrl(info))
+    transporter.close();
+})
 
-    // NB! Store the account object values somewhere if you want
-    // to re-use the same account for future mail deliveries
-
-    // Create a SMTP transporter object
-    let transporter = nodemailer.createTransport(
-       {
-           host: 'smtp.gmail.com',
-           port: 465,
-           secure: true,
-           auth: {
-               user: 'TopShopPlopper@gmail.com',
-               pass: 'TopShopPloppers1'
-           },
-           logger: false,
-           debug: false // include SMTP traffic in the logs
-       },
-        {
-            // default message fields
-
-            // sender info
-            from: 'Pangalink <TopShopPlopper@gmail.com>',
-            headers: {
-                'X-Laziness-level': 1000 // just an example header, no need to use this
-            }
-        }
-    );
-
-    // Message object
-    let message = {
-        // Comma separated list of recipients
-        to: 'Andrew <goullet86@gmail.com>',
-
-        // Subject of the message
-        subject: 'Your Order was Recieved!',
-
-        // plaintext body
-        text: 'Thanks for your order with TopShop!',
-
-        // HTML body
-        html:
-            '<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>' +
-            '<p>Here\'s a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>',
-
-        // An array of attachments
-        attachments: [
-            // String attachment
-            {
-                filename: 'notes.txt',
-                content: 'receipt',
-                contentType: 'text/plain' // optional, would be detected from the filename
-            },
-
-            // Binary Buffer attachment
-            {
-                filename: 'image.png',
-                content: Buffer.from(
-                    'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
-                        '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
-                        'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC',
-                    'base64'
-                ),
-
-                cid: 'note@example.com' // should be as unique as possible
-            },
-
-            // File Stream attachment
-            // {
-            //     filename: 'nyan cat ✔.gif',
-            //     path: __dirname + '/assets/nyan.gif',
-            //     cid: 'nyan@example.com' // should be as unique as possible
-            // }
-        ]
-    };
-
-    transporter.sendMail(message, (error, info) => {
-        if (error) {
-            console.log('Error occurred');
-            console.log(error.message);
-            return process.exit(1);
-        }
-
-        console.log('Message sent successfully!');
-        console.log(nodemailer.getTestMessageUrl(info));
-
-        // only needed when using pooled connections
-        transporter.close();
-    });
-});
+module.exports = orderedEmail;
