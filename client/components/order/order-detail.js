234f3974892409911c99ac38136a3ba966218afc
Cody Fayolle <cfayolle@umich.edu>
2018-04-24 21:58:25 -0500

    closes #249

--- client/components/order/order-detail.js
+++ client/components/order/order-detail.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const OrderDetail = (props) => {
   return (
     <div>
     <span onClick={props.handleClick}>Collapse Details</span>
       {
-        props.products.map(product => <p key={product.id} >{product.id}</p>)
+        props.products.map(product => {
+          let lineItem = props.lineItems.filter(item => item.productId === product.id)
+          lineItem = lineItem[0]
+          console.log(lineItem)
+        return (
+          <div key={product.id}>
+            <br />
+            <Link to={`/products/${product.id}`}><div>{product.name}</div></Link>
+            <div>Purchase Price: {lineItem.purchasePrice}</div>
+            <div>Quantity: {lineItem.quantity}</div>
+            <br />
+          </div>
+        )
+      })
       }
     </div>
   )
