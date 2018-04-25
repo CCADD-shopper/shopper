9a1a2e6b00723a2499e979d88e5024bf00fee754
David Fudacz <davidfudacz@gmail.com>
2018-04-25 07:16:16 -0500

    closes #192

--- client/components/order/order-list.js
+++ client/components/order/order-list.js
@@ -2,17 +2,45 @@ import React from 'react';
 import { connect } from 'react-redux';
 import OrderItem from './order-item';
 
-const OrderList = (props) => {
-  const { orderList, account, user } = props;
-  return (
+class OrderList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      selectValue: 'all',
+    }
+  }
 
-    <div className="orderList">
-      {account
-      ? orderList.filter(order => order.userId === user.id).map(order => <OrderItem key={order.id} order={order} account={account} />)
-      : orderList.map(order => <OrderItem key={order.id} order={order} />)
-      }
-    </div>
-  );
+  handleChange = (event) => {
+    this.setState({
+      selectValue: event.target.value,
+    })
+  }
+
+  render() {
+    const { orderList, account, user } = this.props;
+    const orderStatusOptions = ['all', 'pending', 'processing', 'cancelled', 'completed']
+    return (
+      <div className="orderList">
+        {
+          account
+          ? orderList.filter(order => order.userId === user.id).map(order => <OrderItem key={order.id} order={order} account={account} />)
+          : <div>
+            Filter By Status: <select onChange={this.handleChange} >
+              {
+                orderStatusOptions.map(option => <option key={option} value={option}>{option}</option>)
+              }
+            </select>
+            {
+              orderList.filter(order => {
+                if (this.state.selectValue !== 'all') return order.status === this.state.selectValue
+                else return true
+              }).map(order => <OrderItem key={order.id} order={order} />)
+            }
+          </div>
+        }
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = ({ orderList, user }) => ({ orderList, user });
